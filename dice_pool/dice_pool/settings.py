63c40fbf052e0e7d876b918530a544c28c05b91a
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2013-05-08 12:20:00 -0400

    Added fabric script, misct placeholders

--- dice_pool/dice_pool/settings.py
+++ dice_pool/dice_pool/settings.py
@@ -120,11 +120,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'south',
-	'bestiary'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'bestiary'
 )
 
 # A sample logging configuration. The only tangible logging
