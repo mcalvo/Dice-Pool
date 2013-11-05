b2196c415a90c6b45ceb5b78ee42da12bd25683b
Michael Calvo <mcalvo02@gmail.com>
2013-11-05 15:16:13 -0500

    Clickable dice rolls on monsters through JS magic and regex. GLORIOUS.

---
+++ dice_pool/bestiary/templatetags/diceTags.py
@@ -0,0 +1,11 @@
+from django.template import Library
+import re
+register = Library()
+
+@register.simple_tag
+def damageLineRollCall(damageLine):
+   rollPattern = re.compile("([0-9]*)d([0-9]+|%)([0-9,+,-]*)") 
+   breaks = rollPattern.search(damageLine).groups()
+   if len(breaks) == 2:
+      breaks.append("+ 0")
+   return 'dice.d%s(%s, true), %s' % (breaks[1], breaks[0], breaks[2])
