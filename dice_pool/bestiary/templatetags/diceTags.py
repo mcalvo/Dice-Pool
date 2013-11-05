930a183c2ee6079a30c288b9647f7c0a2fa059cc
Michael Calvo <mcalvo02@gmail.com>
2013-11-05 15:26:27 -0500

    Fixing bug with Minion damage messing up roll link. Can roll for static minion damage. Will probably fix in future.

--- dice_pool/bestiary/templatetags/diceTags.py
+++ dice_pool/bestiary/templatetags/diceTags.py
@@ -5,7 +5,10 @@ register = Library()
 @register.simple_tag
 def damageLineRollCall(damageLine):
    rollPattern = re.compile("([0-9]*)d([0-9]+|%)([0-9,+,-]*)") 
-   breaks = rollPattern.search(damageLine).groups()
+   breaks = rollPattern.search(damageLine)
+   if breaks is None:
+      return "%s , +0" % damageLine
+   breaks = breaks.groups()
    if len(breaks) == 2:
       breaks.append("+ 0")
    return 'dice.d%s(%s, true), %s' % (breaks[1], breaks[0], breaks[2])
